a77c3cd58431c3bd835e9fd8332152958211db4c
TanishqSingla <tanishqsingla00@gmail.com>
2021-09-02 01:39:33 +0530

    Added inputs from operators and numbers

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-const screenOutput = document.getElementById('output');
-
-let expression = screenOutput.innerHTML;
-
+"use strict";
+var screenOutput = document.getElementById("output");
+var expression = screenOutput.innerHTML;
+var opButtons = document.querySelectorAll("button[data-op]");
+var numButtons = document.querySelectorAll('button[data-num]');
+opButtons.forEach(function (opButton) {
+    var sign = opButton.getAttribute("data-op");
+    opButton.addEventListener("click", function () {
+        expression += sign;
+        screenOutput.innerHTML = expression;
+    });
+});
+numButtons.forEach(function (numButton) {
+    var num = numButton.getAttribute('data-num');
+    numButton.addEventListener('click', function () {
+        expression += num;
+        screenOutput.innerHTML = expression;
+    });
+});
 function clearscreen() {
     expression = "";
     screenOutput.innerHTML = expression;
 }
-
 function correctExpression(str) {
-    let newstr = "";
-    for(let i = 0; i < str.length; i++) {
-        if(str[i] == "x") {
+    var newstr = "";
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == "x") {
             newstr += "*";
-        } else if(str[i] == "÷") {
-            newstr += "/"
-        } else {
-            newstr += str[i]
+        }
+        else if (str[i] == "÷") {
+            newstr += "/";
+        }
+        else {
+            newstr += str[i];
         }
     }
     return newstr;
 }
-
 function calculate() {
-    let correctedExpression = correctExpression(expression);
-    let result = eval(correctExpression);
-    screenOutput.innerHTML = result;
+    var correctedExpression = correctExpression(expression);
+    try {
+        var result = eval(correctedExpression);
+        screenOutput.innerHTML = result;
+        expression = result;
+    }
+    catch (err) {
+        if (err) {
+            screenOutput.innerHTML = "Invalid input";
+            expression = "";
+        }
+    }
 }
